d500b1c074f4bf2427306ad45d47099a881949d0
小智 <583172396@qq.com>
2019-06-29 17:40:45 +0800

    监控dao执行时间

--- bucks/src/main/java/com/xz/bucks/BucksApplication.java
+++ bucks/src/main/java/com/xz/bucks/BucksApplication.java
@@ -2,12 +2,14 @@ package com.xz.bucks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableTransactionManagement
+@EnableAspectJAutoProxy
 public class BucksApplication {
 
 	public static void main(String[] args) {
