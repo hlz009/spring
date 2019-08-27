c33c90c05f6d52f19574672923bc21f16b3a0308
小智 <583172396@qq.com>
2019-08-27 13:37:14 +0800

    springboot自动配置示例

---
+++ greeting-autoconfigure/src/test/java/com/xz/greetingautoconfigure/GreetingAutoconfigureApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xz.greetingautoconfigure;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingAutoconfigureApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
